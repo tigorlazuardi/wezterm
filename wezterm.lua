7ebaa2b6490b6741becc1b882acb5b985f0ddc84
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2022-04-23 09:32:11 +0700

    update

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,9 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("JetBrainsMono Nerd Font Mono"),
-	default_prog = { "tmux", "attach" },
+	font = wezterm.font_with_fallback({ "Comic Code Ligatures", "Symbols Nerd Font" }),
+	default_prog = { "bash", "-c", "tmux attach || tmux" },
 	hide_tab_bar_if_only_one_tab = true,
 	window_close_confirmation = "NeverPrompt",
-	initial_cols = 999,
-	initial_rows = 999,
 	color_scheme = "Catppuccin",
 }
