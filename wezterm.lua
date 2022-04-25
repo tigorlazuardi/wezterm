30ee578f6e4de8617a8e7316313beca9c99d4012
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2022-04-25 23:34:46 +0700

    feat(wezterm): set default_prog to os agnostic

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,15 @@
 local wezterm = require("wezterm")
 
+local is_windows = package.config:sub(1, 1) == "\\"
+local default_prog = { "zsh", "-c", "tmux attach || tmux" }
+
+if is_windows then
+	default_prog = { "wsl.exe", "zsh", "-c", "tmux attach || tmux" }
+end
+
 return {
-	font = wezterm.font_with_fallback({ "Comic Code Ligatures", "Symbols Nerd Font" }),
-	default_prog = { "bash", "-c", "tmux attach || tmux" },
+	font = wezterm.font_with_fallback({ "Comic Code Ligatures", "codicon", "Symbols Nerd Font" }),
+	default_prog = default_prog,
 	hide_tab_bar_if_only_one_tab = true,
 	window_close_confirmation = "NeverPrompt",
 	color_scheme = "Catppuccin",
